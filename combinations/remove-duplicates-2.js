78e4390ade15b0464a6fc22ac4e65901e78e1632
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.attlocal.net>
2020-08-26 08:30:24 -0700

    Remove duplicate

---
+++ combinations/remove-duplicates-2.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 0;
+    let prevNum = 'a';
+    let dupCount = 0;
+    for (let j = i; j < nums.length; j++) {
+        if (dupCount < 2 && prevNum === nums[j]) {
+            // it's fine 
+            // add count and keep going
+            dupCount += 1;
+        } else if (dupCount < 2 && prevNum !== nums[j]) {
+            prevNum = nums[j];
+            dupCount = 1;
+            i = j;
+        } else if (dupCount >= 2 && prevNum === nums[j]) {
+            nums[j] = 'a';
+        } else {
+            prevNum = nums[j];
+            dupCount = 1;
+            i = j;
+        }
+    }
+
+    while (nums.indexOf('a') !== -1) {
+        const ind = nums.indexOf('a');
+        nums.splice(ind, 1);
+    }
+    return nums;
+};
+
+// console.log(removeDuplicates([1, 1, 1, 2, 2, 3]));
+console.log(removeDuplicates([0,0,1,1,1,1,2,3,3]));

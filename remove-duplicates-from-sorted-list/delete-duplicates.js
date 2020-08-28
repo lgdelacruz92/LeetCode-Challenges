e1026f8f7a09c01620f4f2c17cc3b34ef7487fbf
Dela Cruz <ldelacruz@ZHOBM-FZQ0UJL414.attlocal.net>
2020-08-28 05:09:08 -0700

    extracted node object

--- remove-duplicates-from-sorted-list/delete-duplicates.js
+++ remove-duplicates-from-sorted-list/delete-duplicates.js
@@ -1,60 +1,8 @@
-class Node {
-    constructor(val) {
-        if (typeof val !== 'number') {
-            throw Error('Node must have number initializer')
-        }
-        this.val = val;
-        this.next = null;
-    }
+const node = require('../lib/node');
 
-    /**
-     * Sets the next node
-     * @param {Node}
-     */
-    setNext(next) {
-        if (!(next instanceof Node)) {
-            throw Error('next: must be an instance of Node');
-        }
-        this.next = next;
-    }
-}
-
-/**
- * Makes a list from array of numbers
- * @param {Array} a Array of numbers
- * @return {Node} head of the linked list
- */
-function makeList(a) {
-    if (a.length === 0) return null;
-    if (a.length === 1) return new Node(a[0]);
-
-    const head = new Node(a[0]);
-    let cur = head;
-    for (let i = 1; i < a.length; i++) {
-        const newNode = new Node(a[i]);
-        cur.next = newNode;
-        cur = newNode;
-    }
-    return head;
-}
-
-/**
- * Prints the linked list in order
- * @param {Node} head 
- * @return {void}
- */
-function print(head) {
-    let cur = head;
-    let arr = [];
-    while (cur !== null) {
-        arr.push(cur.val);
-        cur = cur.next;
-    }
-    console.log(arr);
-}
-
-// print(makeList([1,2]));
-// print(makeList([]));
+const Node = node.Node;
+const makeList = node.makeList;
+const print = node.print;
 
 /**
  * Deletes the duplicates in the linked list
